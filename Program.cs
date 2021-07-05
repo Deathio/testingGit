eaeff295fc76d2a8143ad374514610101f158dc1
Lucas Santos <ls.andrade2804@gmail.com>
2021-07-05 13:32:46 -0300

    OI

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TestProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = new DateTime(2021, 9, 12);
+            DateTime date2 = new DateTime(1980, 2, 11);
+            TimeSpan time = date.Subtract(date2);
+
+            Console.WriteLine($"{Math.Floor((float)(time.Days / 360.0))}-{Math.Floor((float)((time.Days % 360) / 30))}-{Math.Floor((float)(time.Days % 360) % 30)}");
+        }
+    }
+}
