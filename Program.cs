9b96b8db1dcbb05834bbfef1d6538b656883d071
Lucas Santos <ls.andrade2804@gmail.com>
2021-07-06 14:44:50 -0300

    upgrading the code

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace TestProgram
             DateTime date2 = new DateTime(1980, 2, 11);
             TimeSpan time = date.Subtract(date2);
 
-            Console.WriteLine($"{Math.Floor((float)(time.Days / 360.0))}-{Math.Floor((float)((time.Days % 360) / 30))}-{Math.Floor((float)(time.Days % 360) % 30)}");
+            Console.WriteLine($"{Math.Floor((float)(time.Days / 360.0))} years, {Math.Floor((float)((time.Days % 360) / 30))} months, {Math.Floor((float)(time.Days % 360) % 30)} days.");
         }
     }
 }
