4a7dc825889967c2655dae63f2211346952ce1cb
Lucas Santos <ls.andrade2804@gmail.com>
2021-07-06 15:04:37 -0300

    Adding string

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace TestProgram
             DateTime date2 = new DateTime(1980, 2, 11);
             TimeSpan time = date.Subtract(date2);
 
+            Console.Write("Differences: ");
             Console.WriteLine($"{Math.Floor((float)(time.Days / 360.0))} years, {Math.Floor((float)((time.Days % 360) / 30))} months, {Math.Floor((float)(time.Days % 360) % 30)} days.");
         }
     }
